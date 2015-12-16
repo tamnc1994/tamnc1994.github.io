47434280a6e0490f479909629f71c7293bb3e697
Nguyễn Chí Tâm <tamnc1994@gmail.com>
2015-12-16 10:59:47 -0800

    update ex 6

--- BaiTap6/public/js/function.js
+++ BaiTap6/public/js/function.js
@@ -1,32 +1,64 @@
-// JavaScript Document
-function show(x)
+//function change event top down TOP 3 MOVIE REVIEWS
+//x :1 <=> MOVIE1
+//x :2 <=> MOVIE2
+//x :3 <=> MOVIE3
+function changeTopDown(movie)
 {
-	switch(x)
+	switch(movie)
 	{
 		case 1:
-		    document.getElementById("movie_1").style.display= "block";
-            document.getElementById("img1").src="public/images/top.gif";
-            document.getElementById("movie_2").style.display = "none";
-            document.getElementById("img2").src="public/images/top2.gif";
-            document.getElementById("movie_3").style.display = "none";
-            document.getElementById("img3").src="public/images/top2.gif";
+		    var image = document.getElementById('img1');
+			if (image.src.match("top2")) {
+				document.getElementById("movie_1").style.display= "block";
+				document.getElementById("img1").src="public/images/top.gif";
+				document.getElementById("movie_2").style.display = "none";
+				document.getElementById("img2").src="public/images/top2.gif";
+				document.getElementById("movie_3").style.display = "none";
+				document.getElementById("img3").src="public/images/top2.gif";
+			} else {
+				document.getElementById("movie_1").style.display= "none";
+				document.getElementById("img1").src="public/images/top2.gif";
+				document.getElementById("movie_2").style.display = "none";
+				document.getElementById("img2").src="public/images/top2.gif";
+				document.getElementById("movie_3").style.display = "none";
+				document.getElementById("img3").src="public/images/top2.gif";
+			}
 			break;
 		case 2:
-		    document.getElementById("movie_2").style.display= "block";
-            document.getElementById("img2").src="public/images/top.gif";
-            document.getElementById("movie_1").style.display = "none";
-            document.getElementById("img1").src="public/images/top2.gif";
-            document.getElementById("movie_3").style.display = "none";
-            document.getElementById("img3").src="public/images/top2.gif";
+		    var image = document.getElementById('img2');
+			if (image.src.match("top2")) {
+				document.getElementById("movie_2").style.display= "block";
+				document.getElementById("img2").src="public/images/top.gif";
+				document.getElementById("movie_1").style.display = "none";
+				document.getElementById("img1").src="public/images/top2.gif";
+				document.getElementById("movie_3").style.display = "none";
+				document.getElementById("img3").src="public/images/top2.gif";
+			} else {
+				document.getElementById("movie_2").style.display= "none";
+				document.getElementById("img2").src="public/images/top2.gif";
+				document.getElementById("movie_1").style.display = "none";
+				document.getElementById("img1").src="public/images/top2.gif";
+				document.getElementById("movie_3").style.display = "none";
+				document.getElementById("img3").src="public/images/top2.gif";
+			}
 			break;
 		case 3:
-		    document.getElementById("movie_3").style.display= "block";
-            document.getElementById("img3").src="public/images/top.gif";
-            document.getElementById("movie_1").style.display = "none";
-            document.getElementById("img1").src="public/images/top2.gif";
-            document.getElementById("movie_2").style.display = "none";
-            document.getElementById("img2").src="public/images/top2.gif";
-			break;
-		
+		    var image = document.getElementById('img3');
+			if (image.src.match("top2")) {
+				document.getElementById("movie_3").style.display= "block";
+				document.getElementById("img3").src="public/images/top.gif";
+				document.getElementById("movie_2").style.display = "none";
+				document.getElementById("img2").src="public/images/top2.gif";
+				document.getElementById("movie_1").style.display = "none";
+				document.getElementById("img1").src="public/images/top2.gif";
+			} else {
+				document.getElementById("movie_3").style.display= "none";
+				document.getElementById("img3").src="public/images/top2.gif";
+				document.getElementById("movie_2").style.display = "none";
+				document.getElementById("img2").src="public/images/top2.gif";
+				document.getElementById("movie_1").style.display = "none";
+				document.getElementById("img1").src="public/images/top2.gif";
+			}
+			break;	
 	}
 }
